e6987c114e030d6dfbf1c7b032a6ad349273118c
roshan <shrestharoshan768@gmail.com>
2020-12-26 17:02:08 +0545

    Added Cuda folder with both normal and gausion matrices using cuda

---
+++ 6cs005_PortfolioS1_20_21_2039208_Roshan_Shrestha/Source File/Extras/question1_5.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+void main(){
+	int A =2;
+	int D = 4;
+	int B, C, Btemp,Ctemp;
+	
+	Btemp=A+C;
+	B=Ctemp+D;
+	C=Btemp+D;
+	
+	printf("B => %d \n", B );
+	printf("C => %d\n", C);
+	
+}
